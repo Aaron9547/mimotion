80f08f76fc1bc6e265d4b65d8f4d760f14797efa
Aaron9547 <76583372+Aaron9547@users.noreply.github.com>
2025-08-08 14:02:35 +0800

    Update zepplife.py

--- service/zepplife.py
+++ service/zepplife.py
@@ -158,6 +158,7 @@ class ZeppLife:
     def start(self, account: str, password: str, step: int = None):
         """ 启动刷步 """
         hour = time.strptime(time.ctime()).tm_hour
+        print(f"当前时间 [{hour}]  不宜同步。建议同步时间：8:00 ~ 21:00。")
         if not 7 <= hour <= 22:
             raise Exception('当前时间不宜同步。建议同步时间：8:00 ~ 21:00')
         account = f'+86{account}' if re.fullmatch(phone_regex, account) else account
